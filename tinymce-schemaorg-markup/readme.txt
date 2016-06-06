1bbde4bbbc825a37eeec210d9d3d446c3bdc0ca1
marvin <marvin@ecwid.com>
2016-06-06 16:34:02 +0400

    initial commit

---
+++ tinymce-schemaorg-markup/readme.txt
@@ -0,0 +1,21 @@
+=== Protect schema.org markup in HTML editor ===
+Contributors: ecwid
+Tags: schema.org, microdata, rich snippets, tinymce, html editor
+Requires at least: 4.0
+Tested up to: 4.5
+Stable tag: 0.1
+
+Easy tool to stop HTML editor from removing schema.org/microdata tags from post or page content.
+
+== Description ==
+
+Wordpress HTML editor (tinyMCE) treats schema.org attributes like itemscope/itemtype/itemprop as invalid HTML attributes and strips them when you save the post or page content. This plugin alters this behavior and prevent the Wordpress HTML editor from removing the schema.org/microdata markup.
+
+== Installation ==
+
+Just install the plugin as usually and activate it. Once activated, it will keep your markup from being removed by the HTML editor. No setup required.
+
+== Changelog ==
+
+= 0.1 =
+Initial version
---
+++ tinymce-schemaorg-markup/tinymce-schemaorg-markup.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Plugin Name: Protect schema.org markup in HTML editor
+Description: Easy tool to stop HTML editor from removing schema.org/microdata tags from post or page content.
+Author: Ecwid Team
+Author URI: http://www.ecwid.com?source=tinymce-schemaorg-markup
+Version: 0.1
+*/
+
+function tsm_get_extended_valid_elements() {
+	$elements = array(
+		'@' => array(
+			'id',
+			'class',
+			'style',
+			'title',
+			'itemscope',
+			'itemtype',
+			'itemprop',
+			'datetime',
+			'rel'
+		),
+		'div', 'dl', 'dt', 'dd', 'ul', 'li', 'span',
+		'a' => array(
+			'href',
+			'name',
+			'target',
+			'rev',
+			'charset',
+			'lang',
+			'tabindex',
+			'accesskey',
+			'type',
+			'class',
+			'onfocus',
+			'onblur'
+		),
+		'meta' => array(
+			'content'
+		),
+		'link' => array(
+			'href'
+		),
+		'time' => array(
+			'itemprop'
+		)
+	);
+
+	return apply_filters( 'tsm_extended_valid_elements', $elements );
+}
+
+function tsm_tinymce_init( $settings )
+{
+	if( !empty( $settings['extended_valid_elements'] ) ) {
+		$settings['extended_valid_elements'] .= ',';
+	}
+
+	$result = $settings['extended_valid_elements'];
+
+	$elements = tsm_get_extended_valid_elements();
+
+	foreach ( $elements as $key => $element ) {
+		if ( is_array( $element ) && !empty( $key ) ) {
+			$name = $key;
+			$attributes = $element;
+		} else {
+			$name = $element;
+			$attributes = array();
+		}
+
+		if ( !empty( $result ) ) {
+			$result .= ',';
+		}
+
+		$result .= $name;
+
+		if ( !empty( $attributes ) ) {
+			$result .= '[' . implode('|', $attributes) . ']';
+		}
+
+	}
+
+	$settings['extended_valid_elements'] = $result;
+	$settings['valid_children'] .= '+body[meta],+div[meta],+body[link],+div[link]';
+
+	return $settings;
+}
+
+add_filter( 'tiny_mce_before_init', 'tsm_tinymce_init' );
